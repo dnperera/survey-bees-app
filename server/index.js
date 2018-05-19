f4863f64ef235e3703944c1481b383edb5f19fe8
Dayan Perera <dayannp@gmail.com>
2018-05-18 23:14:44 -0700

    install mongoose and set up mongo db connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport');
+const { mongoURI } = require('./config/keys');
 //const authRoutes = require('./routes/authRoutes');
+//
+mongoose.connect(mongoURI);
 
 const app = express();
 //authRoutes(app);

db1a936411de3e3bce11b9bbd8971a223258891f
Dayan Perera <dayannp@gmail.com>
2018-05-19 22:41:22 -0700

    implement serialize and deserialize user

--- server/services/passport.js
+++ server/services/passport.js
@@ -6,6 +6,17 @@ const { googleClientID, googleClientSecret } = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+	// this is how passport create unique session id (token) for each user
+	done(null, user.id); // user.id means mongo id of user's collection
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
+
 passport.use(
 	new GoogleStrategy(
 		{

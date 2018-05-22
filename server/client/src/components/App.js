63f0a6a02ab350c86d3a233e667ad9c122496e08
Dayan Perera <dayannp@gmail.com>
2018-05-22 10:25:59 -0700

    initial setup for Redux

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>Survey New</h2>;
+const Landing = () => <h2>Welcome to Email Survey</h2>;
 
 const App = () => {
-	return <div>Welcome to SurveyBees!!!!</div>;
+	return (
+		<div>
+			<BrowserRouter>
+				<div>
+					<Route path="/" component={Landing} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import App from './components/App';
 
-const store = createStore(() => [], {}, applyMiddleware());
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
 	<Provider store={store}>

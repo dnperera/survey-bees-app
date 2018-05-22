a00a13a3cecf89231346ad71530eee61d69e2660
Dayan Perera <dayannp@gmail.com>
2018-05-21 23:25:37 -0700

    initial React setup with Redux

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);

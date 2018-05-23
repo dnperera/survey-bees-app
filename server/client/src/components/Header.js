1d65e8a89938ece16d452b20096030add14a3b27
Dayan Perera <dayannp@gmail.com>
2018-05-22 22:21:08 -0700

    add login and log out link to the header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<li>
+						<a href="/auth/google">Login With Google </a>
+					</li>
+				);
+			default:
+				return (
+					<li>
+						<a href="/api/logout">Log Out</a>
+					</li>
+				);
+		}
+	}
+
 	render() {
+		console.log(this.props);
 		return (
 			<nav>
 				<div className="nav-wrapper">
 					<a href="#" className="left brand-logo">
 						SurveyBees
 					</a>
-					<ul className="right">
-						<li>
-							<a>Login with Google</a>
-						</li>
-					</ul>
+
+					<ul className="right">{this.renderContent()}</ul>
 				</div>
 			</nav>
 		);
 	}
 }
 
-export default Header;
+//ES6 version
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+// function mapStateToProps(state) {
+// 	return {
+// 		auth: state.auth,
+// 	};
+// }
+export default connect(mapStateToProps)(Header);
 
 /*
   <div class="nav-wrapper">
